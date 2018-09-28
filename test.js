62040ccaa74da6474242d3da81ea63202e982921
njdom24 <Dom32400@aol.com>
2018-09-28 12:36:53 -0400

    Changed to external JS, added hyperlink & more CSS

--- test.js
+++ test.js
@@ -1,2 +1,8 @@
-x = 5;
-print(x);
+var x = 5;  
+        var arr = ["Fried Chicken", "Ramen"];
+        document.body.innerHTML += "<p style = \"margin:0 20\";>hi</p>"
+        document.body.innerHTML += typeof(5) === typeof(29.2);
+        s = "<h1>" + arr.join(" and ");
+        //s = "<a style = \"color:blue\" href = altpage.html>" + s + "</a>"
+        s = "<a href = index.html>" + s + "</a> </h1>"
+        document.body.innerHTML += s;
