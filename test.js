6d071a35d140820edd8a00c3282d7ababcfc218d
njdom24 <Dom32400@aol.com>
2018-10-02 14:47:50 -0400

    Added second page with tables and local gif

--- test.js
+++ test.js
@@ -4,5 +4,5 @@ var x = 5;
         document.body.innerHTML += typeof(5) === typeof(29.2);
         s = "<h1>" + arr.join(" and ");
         //s = "<a style = \"color:blue\" href = altpage.html>" + s + "</a>"
-        s = "<a href = index.html>" + s + "</a> </h1>"
+        s = "<a href = page2.html>" + s + "</a> </h1>"
         document.body.innerHTML += s;
